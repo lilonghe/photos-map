37c3c6d5018f5ec0db8fbdb0804e1679c8fdb420
longhe.li <lilonghe@outlook.com>
2025-04-16 10:25:23 +0800

    feat: use proxy service

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+const BASE_URL = 'https://service.lilonghe.net/proxy';
+
 export function gps2Addr(longitude: string, latitude: string): Promise<string> {
     return gps2AddrDetail(longitude, latitude).then(res => res?.formatted_address)
 }
@@ -5,7 +7,7 @@ export function gps2Addr(longitude: string, latitude: string): Promise<string> {
 export function gps2AddrDetail(longitude: string, latitude: string): Promise<any> {
     return new Promise((resolve) => {
         if (longitude) {
-            fetch(`https://restapi.amap.com/v3/geocode/regeo?output=json&location=${longitude},${latitude}&key=ff15973c3f351d346d4482ca45258110&radius=1000&extensions=all`)
+            fetch(BASE_URL + `/api/map/geocode/regeo?longitude=${longitude}&latitude=${latitude}`)
             .then(res => res.json())
             .then(data => {
                 resolve(data?.regeocode)
